bff8c01774756be07b8dda7fbffd10f01fdc4af7
shekhu-tech <61544121+shekhu-tech@users.noreply.github.com>
2025-03-28 13:00:02 +0530

    Update script.js

--- script.js
+++ script.js
@@ -90,20 +90,6 @@ document.addEventListener("DOMContentLoaded", () => {
 
     setInterval(showNextSlide, 3000); // Slide changes every 3 seconds
 });
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.0.0/firebase-app.js";
-import { getDatabase, ref, get, set, push, remove } from "https://www.gstatic.com/firebasejs/10.0.0/firebase-database.js";
-
-// Firebase Configuration
-const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_AUTH_DOMAIN",
-  databaseURL: "YOUR_DATABASE_URL",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_STORAGE_BUCKET",
-  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-  appId: "YOUR_APP_ID"
-};
-
 
 
  
